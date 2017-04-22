4d96c6e1ecef11b728b13ec6f57ea6c6a6117832
root <gengtianyou@gmail.com>
2017-04-22 15:13:02 +0800

    update

--- test/client.c
+++ test/client.c
@@ -18,13 +18,18 @@ int main(int argc, char *argv[])
 	}
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == -1) return -1;
+	if (sock == -1) {
+        perror("socket()");
+        return -1;
+    }
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
 	serv_addr.sin_port = htons(atoi(argv[2]));
 	
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+        perror("connect()");
 		return -1;
+    }
 
 	ret = send(sock, message, strlen(message), 0);
 	printf("message = %s\n", message);
